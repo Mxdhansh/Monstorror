56df7c2da01f34a01f6e06aa4017553932a2a0e8
Medhansh Kokkonda <kokkonmh@mail.uc.edu>
2026-06-12 15:52:34 +0530

    Bi-weekly Game Progress Report -2
    Starting to build the basics of the game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
+
+//Bi-weekly Game Progress Report - 2
